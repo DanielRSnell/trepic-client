f9962f60f52c213ba44fe1c6003eb4f80390f3ed
DanielRSnell <hi@danielsnell.ninja>
2018-12-16 13:09:05 -0500

    update

--- src/lambda/GetHotel.js
+++ src/lambda/GetHotel.js
@@ -1,8 +1,4 @@
 import axios from "axios";
 export function GetHotel(event, context) {
-  return axios
-    .get(`http://data.zumata.com/hotels/BCRb/en_us/long.json`)
-    .then(res => {
-      return JSON.stringify(res.data);
-    });
+  console.log(event, context);
 }
