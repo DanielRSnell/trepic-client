ae81f991b2f64beaea538fae3f3da775b32303a9
DanielRSnell <hi@danielsnell.ninja>
2018-12-28 10:36:25 -0500

    update

--- src/functions/webhook.js
+++ src/functions/webhook.js
@@ -8,7 +8,16 @@ exports.handler = function(event, context, callback) {
   mongoose.connect(
     "mongodb://dninja:Redskins1@ds159772.mlab.com:59772/trepic-admin"
   );
-  callback(null, {
-    statusCode: 200
-  });
+  Hotel.findOne({ name: "Samabe Bali Suites & Villas" })
+    .limit(1)
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      callback(new Error("Something went wrong"));
+    });
 };
