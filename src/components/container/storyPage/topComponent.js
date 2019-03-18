e54c0483dbe0c5771c0d5f2c8caf6a97948479b4
System Administrator <root@Daniels-MBP.lan1>
2019-03-17 21:52:04 -0500

    update

--- src/components/container/storyPage/topComponent.js
+++ src/components/container/storyPage/topComponent.js
@@ -17,14 +17,16 @@ export default class TopComponent extends Component {
     
     return (
       <Container fluid={true} >
-      <Grid columns={2} stackable verticalAlign='middle' style={{background: `linear-gradient(0deg,rgba(0, 0, 0,0.3),rgba(0, 0, 0,0.5)),url(${this.props.story.profile.meta_box.cover[0].full_url}) 50% 50% no-repeat`, height: '60vh', width: '100%'}}>
+      <Grid columns={2} stackable verticalAlign='middle' style={{background: `linear-gradient(0deg,rgba(0, 0, 0,0.5),rgba(0, 0, 0,0.65)),url(${this.props.story.profile.meta_box.cover[0].full_url}) 50% 50% no-repeat`, height: '60vh', width: '100%'}}>
       <Grid.Row verticalAlign='middle' style={{height: '100%'}}>
       <Grid.Column style={{marginLeft: '3rem', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}} textAlign='center' verticalAlign='middle'>
         <h1 style={{color: 'white', fontSize: '4rem', display: 'block'}}>{this.props.story.profile.meta_box.name}</h1>
      
         <h2 style={{color: 'white', fontSize: '2rem'}}>
         {this.props.story.profile.meta_box.location}
-        </h2>
+              </h2>
+              
+              <p style={{color: 'white', fontSize: '1.15rem', marginTop: '1rem', padding: 10, marginBottom: '1rem'}}>{this.props.story.profile.meta_box.about}</p>
         </Grid.Column>
         <Grid.Column />
         </Grid.Row>
